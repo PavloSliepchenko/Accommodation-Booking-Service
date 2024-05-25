077dab9cb9e7d01ab8ffb11a2797f403f80b1bc7
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-05-25 14:46:52 -0500

    Added authentication controller, swagger and manager user to DB using liquibase

--- src/main/java/com/example/accommodationbookingservice/config/SecurityConfig.java
+++ src/main/java/com/example/accommodationbookingservice/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("api/auth/**")
+                                .requestMatchers("auth/**", "/swagger-ui/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
